1f4e750c6f44a182c0ef2fe05e750e0ff3604a0f
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-07-31 19:21:18 +0000

    Update to python3

--- eduabierta_register/forms.py
+++ eduabierta_register/forms.py
@@ -5,8 +5,8 @@ class EduabiertaInfoForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(EduabiertaInfoForm, self).__init__(*args, **kwargs)
         self.fields['phone'].error_messages = {
-            "required": u"Por favor indique su numero de telefono.",
-            "invalid": u"Numero de telefono invalido"
+            "required": "Por favor indique su numero de telefono.",
+            "invalid": "Numero de telefono invalido"
         }
 
     class Meta(object):
--- eduabierta_register/models.py
+++ eduabierta_register/models.py
@@ -5,6 +5,6 @@ USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class EduabiertaInfo(models.Model):
-    user = models.OneToOneField(USER_MODEL, null=False)
+    user = models.OneToOneField(USER_MODEL, null=False, on_delete=models.CASCADE)
 
     phone = models.CharField(verbose_name="Numero de Telefono", max_length=30, blank=False)
